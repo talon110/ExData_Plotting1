04437bf4adcf31b192edca3d588d81429a328f8c
talon110@gmail.com <talon110@gmail.com>
2015-05-10 10:49:35 -0700

    add plot1 and plot2 functions/code, partial plot3 code

--- plot1.r
+++ plot1.r
@@ -1,3 +1,17 @@
 plot1 <- function(path = "./household_power_consumption.txt") {
+    library(dplyr)
     
+    tab5rows <- read.table(path, header = TRUE, nrows = 5, sep = ";")
+    classes <- sapply(tab5rows, class)
+    
+    data <- read.table(path, header = TRUE, sep = ";", na.strings = "?",
+                       colClasses = classes) %>%
+        transform(., Date = as.Date(Date, "%d/%m/%Y")) %>% 
+        filter(., Date == as.Date("1/2/2007", "%d/%m/%Y") | 
+                       Date == as.Date("2/2/2007", "%d/%m/%Y"))
+
+    png(filename = "plot1.png", width = 480, height = 480, units = "px")
+    hist(data$Global_active_power, breaks = 12, main = "Global Active Power", col = "red", 
+         xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+    dev.off()
 }
--- plot3.r
+++ plot3.r
@@ -1,3 +1,18 @@
 plot3 <- function(path = "./household_power_consumption.txt") {
+    library(dplyr)
     
+    tab5rows <- read.table(path, header = TRUE, nrows = 5, sep = ";", stringsAsFactors = F)
+    classes <- sapply(tab5rows, class)
+    
+    data <- read.table(path, header = TRUE, sep = ";", na.strings = "?",
+                       colClasses = classes, stringsAsFactors = F) %>%
+        filter(., Date == "1/2/2007" | Date == "2/2/2007") %>%
+        select(., Date:Sub_metering_3) %>%
+        transform(., Date = strptime(paste(Date, Time, sep = " "), "%d/%m/%Y %H:%M:%S")) %>%
+        select(., -(Time))
+    
+    #png(filename = "plot3.png", width = 480, height = 480, units = "px")
+    plot(data$Date, data$Global_active_power, type = "l", xlab = "",
+         ylab = "Global Active Power (kilowatts)")
+    #dev.off()
 }
